60e7ea3109413a795465e999c4e2044cf3b65d3a
unknown <yassery12345@gmail.com>
2024-05-28 21:10:32 +0300

    first commit

---
+++ Python basic part-1/1.py
@@ -0,0 +1,8 @@
+# print('Twinkle, twinkle, little star,\n	     How I wonder what you are!\n               Up above the world so high,\n                Like a diamond in the sky.\nTwinkle, twinkle, little star,\n           How I wonder what you are')
+
+print('''Twinkle, twinkle, little star,
+	How I wonder what you are! 
+		Up above the world so high,   		
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are''')
