b08f2faf8537424f3c79ed20584960e84c312771
Yasser <yassery12345@gmail.com>
2024-06-04 21:37:12 +0300

    day 6

--- Python basic part-1/20-remove negative.py
+++ Python basic part-1/20-remove negative.py
@@ -1,3 +1,8 @@
-num = '-6, -4, 5'
+# Write a Python program that returns a string that is n (non-negative integer) copies of a given string.
+# num = '-6, -4, 5'
+# print(num.replace('-', ''))
 
-print(num.replace('-', ''))
+text = input('Enter a text: ')
+num = int(input('Enter number of times to print the copy: '))
+
+print(f'{text} ' * num)
